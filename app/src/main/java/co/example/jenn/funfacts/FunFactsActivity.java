e53f356ceefa75bd92f75bb610c5c0b7fbb60301
Jenn Leung <j.leung91@gmail.com>
2014-11-02 15:19:18 -0500

    added random generator for array of facts

--- app/src/main/java/co/example/jenn/funfacts/FunFactsActivity.java
+++ app/src/main/java/co/example/jenn/funfacts/FunFactsActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.*;
 
 
 public class FunFactsActivity extends Activity {
@@ -12,6 +16,36 @@ public class FunFactsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        //Declare our view variables and assign them the views fr layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactbutton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] facts = {
+                    "Ants stretch when they wake up in the morning.",
+                    "Ostriches can run faster than horses.",
+                    "Olympic gold medals are actually made mostly of silver.",
+                    "You are born with 300 bones; by the time you are an adult you will have 206.",
+                    "It takes about 8 minutes for light from the Sun to reach Earth.",
+                    "Some bamboo plants can grow almost a meter in just one day.",
+                    "The state of Florida is bigger than England.",
+                    "Some penguins can leap 2-3 meters out of the water.",
+                    "On average, it takes 66 days to form a new habit.",
+                    "Mammoths still walked the earth when the Great Pyramid was being built." };
+                //The button was clicked, update new fact label with new fact
+                String fact = "";
+
+                //Randomly select fact
+                Random randomGenerator = new Random();
+                int randomNum = randomGenerator.nextInt(10);
+
+                //Update the label with our dynamic fact
+                factLabel.setText(facts[randomNum]);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 
 

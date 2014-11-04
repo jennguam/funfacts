09c5ab8761b3bd97ec6befba6340dad0c491e513
Jenn Leung <j.leung91@gmail.com>
2014-11-03 23:15:31 -0500

    added different bg colors for different facts

--- app/src/main/java/co/example/jenn/funfacts/FunFactsActivity.java
+++ app/src/main/java/co/example/jenn/funfacts/FunFactsActivity.java
@@ -1,17 +1,20 @@
 package co.example.jenn.funfacts;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import java.util.*;
 
 
 public class FunFactsActivity extends Activity {
-
+    private factBook mFactBook = new factBook();
+    private ColorWheel mColorWheel = new ColorWheel();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,52 +22,20 @@ public class FunFactsActivity extends Activity {
 
         //Declare our view variables and assign them the views fr layout file
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
-        Button showFactButton = (Button) findViewById(R.id.showFactbutton);
+        final Button showFactButton = (Button) findViewById(R.id.showFactbutton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String[] facts = {
-                    "Ants stretch when they wake up in the morning.",
-                    "Ostriches can run faster than horses.",
-                    "Olympic gold medals are actually made mostly of silver.",
-                    "You are born with 300 bones; by the time you are an adult you will have 206.",
-                    "It takes about 8 minutes for light from the Sun to reach Earth.",
-                    "Some bamboo plants can grow almost a meter in just one day.",
-                    "The state of Florida is bigger than England.",
-                    "Some penguins can leap 2-3 meters out of the water.",
-                    "On average, it takes 66 days to form a new habit.",
-                    "Mammoths still walked the earth when the Great Pyramid was being built." };
-                //The button was clicked, update new fact label with new fact
-                String fact = "";
-
-                //Randomly select fact
-                Random randomGenerator = new Random();
-                int randomNum = randomGenerator.nextInt(10);
-
+                String fact= mFactBook.getFact();
                 //Update the label with our dynamic fact
-                factLabel.setText(facts[randomNum]);
+                int color = mColorWheel.getColor();
+                factLabel.setText(fact);
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
             }
         };
         showFactButton.setOnClickListener(listener);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.fun_facts, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
